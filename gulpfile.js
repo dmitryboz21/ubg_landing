fdbe02f61fae19ffebc2abebbbb3dfd2a744435f
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-01 11:20:52 +0300

    desktop header and footer

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ gulp.task('js:build', function () {
 		.pipe(reload({stream: true}));
 });
 
-gulp.task('style:build', function () {		
+gulp.task('style:build', function () {
 	return gulp.src(path.src.style)
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
@@ -100,23 +100,23 @@ gulp.task('style:build', function () {
 
 gulp.task('images:build', function () {
 	return gulp.src(path.src.images)
-		.pipe(imagemin({
+		/*.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant()],
 			interlaced: true
-		}))
+		}))*/
 		.pipe(gulp.dest(path.build.images))
 		.pipe(reload({stream: true}));
 });
 gulp.task('img:build', function () {
 	return gulp.src(path.src.img)
-		.pipe(imagemin({
+		/*.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant()],
 			interlaced: true
-		}))
+		}))*/
 		.pipe(gulp.dest(path.build.img))
 		.pipe(reload({stream: true}));
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 /* libs */
 
 //= libs/jquery-3.3.1.min.js
-//= libs/jquery.fancybox.min.js
+
 //= libs/slick.min.js
 
 /* my scripts */
