3d9f1f82f53f84cc9818c1b8e3025fa14e290b9d
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-03 13:15:39 +0300

    add

--- build/js/main.js
+++ build/js/main.js
@@ -12,8 +12,9 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-	$('.sh-burger').click(function () {
+	$('.sh-burger-wrap').click(function () {
 		$(this).toggleClass('sh-burger--active');
+		$('#site-header').toggleClass('site-header--menu-open');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
 
--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
-	$('.sh-burger').click(function () {
+	$('.sh-burger-wrap').click(function () {
 		$(this).toggleClass('sh-burger--active');
+		$('#site-header').toggleClass('site-header--menu-open');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
 	});
 
