4d74818d8d2ffef00d97bc4eb556e81211f12997
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-04 14:38:15 +0300

    add

--- build/js/main.js
+++ build/js/main.js
@@ -13,11 +13,33 @@ $(document).ready(function () {
 
 $(document).ready(function () {
 	$('.sh-burger-wrap').click(function () {
+		// before opening
+		var originalWidth = $('body').width();
+
 		$(this).toggleClass('sh-burger--active');
 		$('#site-header').toggleClass('site-header--menu-open');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
-	});
+		$('body').toggleClass('body-locked');
 
+		if ($('body').hasClass('body-locked')) {
+			if ($('body').width() > originalWidth) {
+				$('body, .js-need-compensate-scrollbar').css("padding-right", ($('body').width() - originalWidth) +"px");
+
+			}
+
+		}
+		else{
+			$('body, .js-need-compensate-scrollbar').css("padding-right", "");
+
+		}
+
+
+	});
+	$('.js-dark-bg-for-menu').click(function(){
+		if ($('body').hasClass('body-locked')) {
+			$('.sh-burger-wrap').click();
+		}
+	});
 });
 $(document).ready(function () {
    // $("input[name='phone']").mask(" +7 (999) 999-99-99");
--- src/js/partials/header.js
+++ src/js/partials/header.js
@@ -1,9 +1,32 @@
 
 $(document).ready(function () {
 	$('.sh-burger-wrap').click(function () {
+		// before opening
+		var originalWidth = $('body').width();
+
 		$(this).toggleClass('sh-burger--active');
 		$('#site-header').toggleClass('site-header--menu-open');
 		$('.sh-menu-wrap').toggleClass('sh-menu-wrap--open');
-	});
+		$('body').toggleClass('body-locked');
 
+		if ($('body').hasClass('body-locked')) {
+			if ($('body').width() > originalWidth) {
+				$('body, .js-need-compensate-scrollbar').css("padding-right", ($('body').width() - originalWidth) +"px");
+
+			}
+
+		}
+		else{
+			$('body, .js-need-compensate-scrollbar').css("padding-right", "");
+
+		}
+
+
+	});
+	$('.js-dark-bg-for-menu').click(function(){
+		if ($('body').hasClass('body-locked')) {
+			$('.sh-burger-wrap').click();
+		}
+	});
 });
+
