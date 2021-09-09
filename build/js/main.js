09390e1812f9aa5e1b1c50e98e52be73f2194230
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-09 23:00:07 +0300

    123

--- build/js/main.js
+++ build/js/main.js
@@ -45,39 +45,51 @@ $(document).ready(function () {
 	});
 });
 $(document).ready(function () {
-   // $("input[name='phone']").mask(" +7 (999) 999-99-99");
+	// $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
 
-   //ссылку на html5 video видео класть в аттрибут href
-   $('.js-popup-play-video').magnificPopup({
-      type: 'iframe',
-      mainClass: 'mfp-fade',
-      removalDelay: 160,
-      preloader: false,
-      fixedContentPos: false,
-      iframe: {
-         markup: '<div class="mfp-iframe-scaler">' +
-            '<div class="mfp-close"></div>' +
-            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-            '</div>',
+	//ссылку на html5 video видео класть в аттрибут href
+	$('.js-popup-play-video').magnificPopup({
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false,
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">' +
+				'<div class="mfp-close"></div>' +
+				'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+				'</div>',
 
-         srcAction: 'iframe_src',
-      }
-   });
-   //в href надо "#id_of_popup"
-   $('.js-open-popup-inline').magnificPopup({
-      type: 'inline',
-      closeBtnInside: true,
-      mainClass: "myPopupMainClass",
-      closeMarkup: '<button title="%title%" type="button" class="mfp-close"><svg class="js-mfp-close-svg" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="42.544" y1="14.2052" x2="14.2052" y2="42.544" stroke="white" stroke-width="2"/><line x1="14.2086" y1="14.1298" x2="42.5474" y2="42.4686" stroke="white" stroke-width="2"/></svg></button>'
+			srcAction: 'iframe_src',
+		}
+	});
+	//в href надо "#id_of_popup"
+	$('.js-open-popup-inline').magnificPopup({
+		type: 'inline',
+		closeBtnInside: true,
+		mainClass: "myPopupMainClass",
+		closeMarkup: '<button title="%title%" type="button" class="mfp-close"><svg class="js-mfp-close-svg" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="42.544" y1="14.2052" x2="14.2052" y2="42.544" stroke="white" stroke-width="2"/><line x1="14.2086" y1="14.1298" x2="42.5474" y2="42.4686" stroke="white" stroke-width="2"/></svg></button>'
 
-   });
+	});
 
-   $('body').on('click', '.js-mfp-close-svg', function(){
-      $(this).parent().click();
-   });
-   $('.js-footer-menu-opener').click(function(){
-      $(this).toggleClass('footer-menu-opener--open')
-      $('.footer-menu-wrap').slideToggle(300);
-   });
+	$('body').on('click', '.js-mfp-close-svg', function () {
+		$(this).parent().click();
+	});
+	$('.js-footer-menu-opener').click(function () {
+		$(this).toggleClass('footer-menu-opener--open')
+		$('.footer-menu-wrap').slideToggle(300);
+	});
+	$('body').on('click', '.js-cf-accordeon-item__top', function () {
+		var item = $(this).closest('.cf-accordeon-item');
+		if (item.hasClass('cf-accordeon-item--active')) {
+			item.removeClass('cf-accordeon-item--active');
+			item.find('.cf-accordeon-item__content').slideUp(400);
+		}
+		else {
+			item.addClass('cf-accordeon-item--active');
+			item.find('.cf-accordeon-item__content').slideDown(400);
+			item.siblings('.cf-accordeon-item--active').removeClass('cf-accordeon-item--active').find('.cf-accordeon-item__content').slideUp(400);
+		}
+	});
 });
