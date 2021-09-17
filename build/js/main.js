3e90590e44cd709d792cf2041a144c27f04a5fde
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-17 20:48:17 +0300

    modals

--- build/js/main.js
+++ build/js/main.js
@@ -65,12 +65,30 @@ $(document).ready(function() {
 			srcAction: 'iframe_src',
 		}
 	});
+
+	$('.js-set-modal-tarif').click(function(){
+		$('#tarif-inp').val($(this).attr('data-tarif'));
+	});
+
+
+
+
 	//в href надо "#id_of_popup"
 	$('.js-open-popup-inline').magnificPopup({
 		type: 'inline',
 		closeBtnInside: true,
 		mainClass: "myPopupMainClass",
-		closeMarkup: '<button title="%title%" type="button" class="mfp-close"><svg class="js-mfp-close-svg" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="42.544" y1="14.2052" x2="14.2052" y2="42.544" stroke="white" stroke-width="2"/><line x1="14.2086" y1="14.1298" x2="42.5474" y2="42.4686" stroke="white" stroke-width="2"/></svg></button>'
+		closeMarkup: '<button title="%title%" type="button" class="mfp-close"><svg class="js-mfp-close-svg" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="42.544" y1="14.2052" x2="14.2052" y2="42.544" stroke="white" stroke-width="2"/><line x1="14.2086" y1="14.1298" x2="42.5474" y2="42.4686" stroke="white" stroke-width="2"/></svg></button>',
+		callbacks: {
+		  open: function() {
+			$.magnificPopup.instance.close = function() {
+				$('#tarif-inp').val('');
+
+			  // Call the original close method to close the popup
+			  $.magnificPopup.proto.close.call(this);
+			};
+		  }
+		}
 
 	});
 
