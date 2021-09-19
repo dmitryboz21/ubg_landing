4d150e58585f349db75eb2ccdfe4f85e51897665
dmitryboz21 <dmitryboz21@yandex.com>
2021-09-19 20:37:46 +0300

    123

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -55,6 +55,14 @@ $(document).ready(function() {
 	});
 
 
+	$('.js-open-video-type-2').magnificPopup({
+		type: 'iframe',
+		closeBtnInside: true,
+		mainClass: "myPopupMainClass myPopupMainClass--video myPopupMainClass--video--type2",
+		closeMarkup: '<button title="%title%" type="button" class="mfp-close"><svg class="js-mfp-close-svg" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg"><line x1="42.544" y1="14.2052" x2="14.2052" y2="42.544" stroke="white" stroke-width="2"/><line x1="14.2086" y1="14.1298" x2="42.5474" y2="42.4686" stroke="white" stroke-width="2"/></svg></button>'
+
+	});
+
 
 	$('body').on('click', '.js-mfp-close-svg', function() {
 		$(this).parent().click();
